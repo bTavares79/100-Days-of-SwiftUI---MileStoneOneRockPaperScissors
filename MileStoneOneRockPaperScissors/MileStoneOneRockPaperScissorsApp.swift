388fc5c669f8a20c9c0e932751b2d6beac8b58ec
Brian Tavares <btavares79@gmail.com>
2023-06-01 09:12:01 -0400

    Initial Commit

---
+++ MileStoneOneRockPaperScissors/MileStoneOneRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileStoneOneRockPaperScissorsApp.swift
+//  MileStoneOneRockPaperScissors
+//
+//  Created by Brian Tavares on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MileStoneOneRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
